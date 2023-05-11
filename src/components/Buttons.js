74b3cfa89ce9e83060a44ab5574a675e94e72aa0
CarlosIgreda <cjigredarivas@gmail.com>
2023-05-11 05:52:56 -0500

    Add components folder

---
+++ src/components/Buttons.js
@@ -0,0 +1,29 @@
+function Buttons() {
+  return (
+    <div className="buttons">
+      <div className="display">
+        0
+      </div>
+      <button className="button numbers-and-extras" type="button">AC</button>
+      <button className="button numbers-and-extras" type="button">+/-</button>
+      <button className="button numbers-and-extras" type="button">%</button>
+      <button className="button math-symbols" type="button">÷</button>
+      <button className="button numbers-and-extras" type="button">7</button>
+      <button className="button numbers-and-extras" type="button">8</button>
+      <button className="button numbers-and-extras" type="button">9</button>
+      <button className="button math-symbols" type="button">×</button>
+      <button className="button numbers-and-extras" type="button">4</button>
+      <button className="button numbers-and-extras" type="button">5</button>
+      <button className="button numbers-and-extras" type="button">6</button>
+      <button className="button math-symbols" type="button">-</button>
+      <button className="button numbers-and-extras" type="button">1</button>
+      <button className="button numbers-and-extras" type="button">2</button>
+      <button className="button numbers-and-extras" type="button">3</button>
+      <button className="button math-symbols" type="button">+</button>
+      <button className="button numbers-and-extras number0" type="button">0</button>
+      <button className="button numbers-and-extras" type="button">.</button>
+      <button className="button math-symbols" type="button">=</button>
+    </div>
+  );
+}
+export default Buttons;
---
+++ src/components/Calculator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/App.css';
+import Buttons from './Buttons';
+
+function Calculator() {
+  return (
+    <div className="calc">
+      <Buttons />
+    </div>
+  );
+}
+export default Calculator;
