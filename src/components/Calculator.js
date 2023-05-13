c65e78a32d2aee9d97a7a2bd6dbea7aac1994657
CarlosIgreda <cjigredarivas@gmail.com>
2023-05-13 03:31:12 -0500

    Update Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import '../styles/App.css';
 import Buttons from './Buttons';
+import QuoteContainer from './QuoteContainer';
 
 function Calculator() {
   return (
-    <div className="calc">
-      <Buttons />
-    </div>
+    <body>
+      <h1 className="title">📐 Math Magicians📏</h1>
+      <div className="calc">
+        <Buttons />
+        <QuoteContainer />
+      </div>
+    </body>
   );
 }
 export default Calculator;
